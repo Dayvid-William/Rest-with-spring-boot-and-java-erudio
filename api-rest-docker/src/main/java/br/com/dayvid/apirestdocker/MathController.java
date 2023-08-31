2cf096f71ebfda9dd9c9c0664f58518bc7e9208d
Dayvid-William <dayvid.william5@outlook.com>
2023-08-31 13:03:07 -0300

    aula 55 create method of convert str number in double number

--- api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/MathController.java
+++ api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/MathController.java
@@ -4,14 +4,27 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
+@RestController //Funciona como @ResponseBody e @Controler cria uma map do model object e encontra uma viwer equivalente
 public class MathController {
 
     private static final String template = "Hello, %s!";
     private static final AtomicLong counter = new AtomicLong();
-    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double sum(@PathVariable(value = "numberOne")String numberOne,
-                      @PathVariable(value = "numberTwo")String numberTwo) {
-        return 1d;
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)// As chaves tornam os parametros obrigatorios
+    public Double sum(@PathVariable(value = "numberOne")String numberOne, //especifica que a variavel vem da url request
+                      @PathVariable(value = "numberTwo")String numberTwo) throws Exception {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String strNumber) {
+        return null;
+    }
+
+    private boolean isNumeric(String strNumber) {
+        return false;
     }
 }
