51d545589bd835bc722319025ce53b5a038104c7
Dayvid-William <dayvid.william5@outlook.com>
2023-08-31 13:26:25 -0300

    aula 56 create sum method and numbers validation methods

--- api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/MathController.java
+++ api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/MathController.java
@@ -21,10 +21,16 @@ public class MathController {
     }
 
     private Double convertToDouble(String strNumber) {
-        return null;
+        if(strNumber == null) return 0D;
+        // BR 10,25 US 10.25
+        String number = strNumber.replaceAll(",", ".");
+        if (isNumeric(number)) return Double.parseDouble(number);
+        return 0D;
     }
 
     private boolean isNumeric(String strNumber) {
-        return false;
+        if(strNumber == null) return false;
+        String number = strNumber.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
