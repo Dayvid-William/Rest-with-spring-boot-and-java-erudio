a7588cf001d2a18c5e9fb6948de9224ecb78bf3c
Dayvid-William <dayvid.william5@outlook.com>
2023-08-31 13:41:41 -0300

    aula 56 parte 01

---
+++ api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/exceptions/UnsuportedMathOperationException.java
@@ -0,0 +1,14 @@
+package br.com.dayvid.apirestdocker.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsuportedMathOperationException extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+}
