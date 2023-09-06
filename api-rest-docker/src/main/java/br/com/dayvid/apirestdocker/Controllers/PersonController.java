f30e920470b9e034d6be93f8e688c6ac41dba5de
Dayvid-William <dayvid.william5@outlook.com>
2023-09-06 01:14:51 -0300

    aula 65 implements method find all

--- api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/Controllers/PersonController.java
+++ api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/Controllers/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController //Funciona como @ResponseBody e @Controler cria uma map do model object e encontra uma viwer equivalente
@@ -14,6 +15,10 @@ public class PersonController {
     //private PersonServices service = new PersonServices();
     @Autowired // O spring boot fica responsavel pela instanciação do objeto de forma dinamica (e preciso ter a anotação @Service ou outra que seja um Alias(ou seja anotações injetadas) no objeto origem)
     private PersonServices service;
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
     @RequestMapping(value = "{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)// As chaves tornam os parametros obrigatorios, produz um JSON
     public Person findById(@PathVariable(value = "id")String id) throws Exception {  //especifica que a variavel vem da url request
         return service.findById(id);
--- api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/services/PersonServices.java
+++ api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/services/PersonServices.java
@@ -3,22 +3,44 @@ package br.com.dayvid.apirestdocker.services;
 import br.com.dayvid.apirestdocker.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 @Service //Serve para o spring boot veja esse como objeto que sera injetado em run time em outras classes da aplicação
 public class PersonServices {
     private static final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+    
+    public List<Person> findAll(){
+        logger.info("Finding all people!");
+        List<Person> persons = new ArrayList<>();
+        for (int i  = 0; i < 8; i++ ) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
 
     public Person findById(String id) {
 
-        logger.info("Finding one person");
+        logger.info("Finding one person!");
 
         Person person = new Person();
         person.setId(counter.incrementAndGet());
         person.setFirstName("Leandro");
         person.setLastName("Costa");
         person.setAddress("Uberlândia - Minas Gerais");
+        person.setGender("Male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brazil " + i);
         person.setGender("Male");
         return person;
     }
