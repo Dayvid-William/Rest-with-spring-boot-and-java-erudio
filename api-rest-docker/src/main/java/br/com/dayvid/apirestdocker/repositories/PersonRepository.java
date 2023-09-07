7de654a9e4c05ad06f6c9765e106ab7eee894587
Dayvid-William <dayvid.william5@outlook.com>
2023-09-07 13:13:21 -0300

    updating repository and readme

--- api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/repositories/PersonRepository.java
+++ api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/repositories/PersonRepository.java
@@ -2,7 +2,7 @@ package br.com.dayvid.apirestdocker.repositories;
 
 import br.com.dayvid.apirestdocker.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository // Indica que o objeto e um repositorio no database que posibilita fazer um CRUD completo no database
+// @Repository  Indica que o objeto e um repositorio no database que posibilita fazer um CRUD completo no database
+// A parti do spring boot 3.0.1 o spring boot entende como um repositorio só de extender o JPARepository
 public interface PersonRepository extends JpaRepository<Person, Long> {}
