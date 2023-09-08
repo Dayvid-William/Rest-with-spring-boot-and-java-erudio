b98eee426eb1443793b46357993a092101f3147d
Dayvid-William <dayvid.william5@outlook.com>
2023-09-08 02:44:24 -0300

    class 80 refactoring services to work with VO(value object) product

--- api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/data/vo/v1/PersonVO.java
+++ api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/data/vo/v1/PersonVO.java
@@ -1,2 +1,88 @@
-package br.com.dayvid.apirestdocker.data.vo.v1;public class PersonVO {
+package br.com.dayvid.apirestdocker.data.vo.v1;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonVO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String gender;
+
+    public PersonVO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonVO personVO = (PersonVO) o;
+
+        if (!Objects.equals(id, personVO.id)) return false;
+        if (!Objects.equals(firstName, personVO.firstName)) return false;
+        if (!Objects.equals(lastName, personVO.lastName)) return false;
+        if (!Objects.equals(address, personVO.address)) return false;
+        return Objects.equals(gender, personVO.gender);
+    }
+
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        result = 31 * result + (gender != null ? gender.hashCode() : 0);
+        return result;
+    }
 }
