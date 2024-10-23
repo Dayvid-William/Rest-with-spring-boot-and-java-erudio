a7c9ebfb62c759e3dcd7b6a48eecdd031c7e79dd
Dayvid William <dayvid.william5@outlook.com>
2024-10-22 21:35:47 -0300

    includ HATEOS tests in the person controle json

--- api-rest-docker/src/test/java/br/com/dayvid/apirestdocker/integrationtests/controller/withjson/PersonControllerJsonTest.java
+++ api-rest-docker/src/test/java/br/com/dayvid/apirestdocker/integrationtests/controller/withjson/PersonControllerJsonTest.java
@@ -315,6 +315,36 @@ public class PersonControllerJsonTest extends AbstractIntegrationTest {
 		assertEquals("Male", foundPersonOne.getGender());
 	}
 
+	@Test
+	@Order(8)
+	public void testHATEOS() throws JsonMappingException, JsonProcessingException {
+
+		var content = given().spec(specification)
+				.contentType(TestConfigs.CONTENT_TYPE_JSON)
+				.accept(TestConfigs.CONTENT_TYPE_JSON)
+				.queryParams("page", 3, "size", 10, "direction", "asc")
+					.when()
+					.get()
+				.then()
+					.statusCode(200)
+					.extract()
+					.body()
+						.asString();
+
+		assertTrue(content.contains("_links\":{\"self\":{\"href\":\"http://localhost:8888/api/person/v1/901\"}}}"));
+		assertTrue(content.contains("_links\":{\"self\":{\"href\":\"http://localhost:8888/api/person/v1/133\"}}}"));
+		assertTrue(content.contains("_links\":{\"self\":{\"href\":\"http://localhost:8888/api/person/v1/410\"}}}"));
+
+		assertTrue(content.contains("\"page\":{\"size\":10,\"totalElements\":1007,\"totalPages\":101,\"number\":3}}"));
+
+		assertTrue(content.contains("\"last\":{\"href\":\"http://localhost:8888/api/person/v1?direction=asc&page=100&size=10&sort=firstName,asc\"}}"));
+		assertTrue(content.contains("\"next\":{\"href\":\"http://localhost:8888/api/person/v1?direction=asc&page=4&size=10&sort=firstName,asc\"}"));
+		assertTrue(content.contains("\"self\":{\"href\":\"http://localhost:8888/api/person/v1?page=3&size=10&direction=asc\"}"));
+		assertTrue(content.contains("\"prev\":{\"href\":\"http://localhost:8888/api/person/v1?direction=asc&page=2&size=10&sort=firstName,asc\"}"));
+		assertTrue(content.contains("{\"first\":{\"href\":\"http://localhost:8888/api/person/v1?direction=asc&page=0&size=10&sort=firstName,asc\"}"));
+
+	}
+
 	private void mockPerson() {
 		person.setFirstName("Nelson");
 		person.setLastName("Piquet");
