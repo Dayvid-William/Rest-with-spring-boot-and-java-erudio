a82ccbf9a2d07e82a74ce68ce6bd1fbe9cf21b84
Dayvid William <dayvid.william5@outlook.com>
2024-10-23 21:53:40 -0300

    implementing HATEOS integration test with XML an YAML

--- api-rest-docker/src/test/java/br/com/dayvid/apirestdocker/integrationtests/controller/withxml/PersonControllerXmlTest.java
+++ api-rest-docker/src/test/java/br/com/dayvid/apirestdocker/integrationtests/controller/withxml/PersonControllerXmlTest.java
@@ -343,6 +343,36 @@ public class PersonControllerXmlTest extends AbstractIntegrationTest {
 		assertEquals("Male", foundPersonOne.getGender());
 	}
 
+	@Test
+	@Order(9)
+	public void testHATEOS() throws JsonMappingException, JsonProcessingException {
+
+		var content = given().spec(specification)
+				.contentType(TestConfigs.CONTENT_TYPE_XML)
+				.accept(TestConfigs.CONTENT_TYPE_XML)
+				.queryParams("page", 3, "size", 10, "direction", "asc")
+					.when()
+					.get()
+				.then()
+					.statusCode(200)
+					.extract()
+					.body()
+						.asString();
+
+		assertTrue(content.contains("<links><rel>self</rel><href>http://localhost:8888/api/person/v1/901</href></links>"));
+		assertTrue(content.contains("<links><rel>self</rel><href>http://localhost:8888/api/person/v1/133</href></links>"));
+		assertTrue(content.contains("<links><rel>self</rel><href>http://localhost:8888/api/person/v1/410</href></links>"));
+
+		assertTrue(content.contains("<page><size>10</size><totalElements>1007</totalElements><totalPages>101</totalPages><number>3</number></page>"));
+
+		assertTrue(content.contains("<links><rel>last</rel><href>http://localhost:8888/api/person/v1?direction=asc&amp;page=100&amp;size=10&amp;sort=firstName,asc</href></links>"));
+		assertTrue(content.contains("<links><rel>next</rel><href>http://localhost:8888/api/person/v1?direction=asc&amp;page=4&amp;size=10&amp;sort=firstName,asc</href></links>"));
+		assertTrue(content.contains("<links><rel>self</rel><href>http://localhost:8888/api/person/v1?page=3&amp;size=10&amp;direction=asc</href></links>"));
+		assertTrue(content.contains("<links><rel>prev</rel><href>http://localhost:8888/api/person/v1?direction=asc&amp;page=2&amp;size=10&amp;sort=firstName,asc</href></links>"));
+		assertTrue(content.contains("<links><rel>first</rel><href>http://localhost:8888/api/person/v1?direction=asc&amp;page=0&amp;size=10&amp;sort=firstName,asc</href></links>"));
+
+	}
+
 	private void mockPerson() {
 		person.setFirstName("Nelson");
 		person.setLastName("Piquet");
