39cad30411aeb2465581b7d384aef8e78552aa66
Dayvid William <dayvid.william5@outlook.com>
2024-11-05 21:54:56 -0300

    Create File Controller

--- api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/services/FileStorageService.java
+++ api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/services/FileStorageService.java
@@ -2,7 +2,10 @@ package br.com.dayvid.apirestdocker.services;
 
 import br.com.dayvid.apirestdocker.config.FileStorageConfig;
 import br.com.dayvid.apirestdocker.exceptions.FileStorageException;
+import br.com.dayvid.apirestdocker.exceptions.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,32 +21,44 @@ public class FileStorageService {
 
     @Autowired
     public FileStorageService(FileStorageConfig fileStorageConfig) {
-
         Path path = Paths.get(fileStorageConfig.getUploadDir())
-                .toAbsolutePath().normalize(); // normalize faz scapes caso nescessario
+                .toAbsolutePath().normalize();
 
         this.fileStorageLocation = path;
 
         try {
             Files.createDirectories(this.fileStorageLocation);
         } catch (Exception e) {
-            throw new FileStorageException("Could not create the directory where the uploaded files will be stored!", e);
+            throw new FileStorageException(
+                    "Could not create the directory where the uploaded files will be stored!", e);
         }
     }
 
     public String storeFile(MultipartFile file) {
         String filename = StringUtils.cleanPath(file.getOriginalFilename());
         try {
+            // Filename..txt
             if (filename.contains("..")) {
-                throw new FileStorageException("Sorry! file name contains invalid path sequence " + filename);
+                throw new FileStorageException(
+                        "Sorry! Filename contains invalid path sequence " + filename);
             }
-
             Path targetLocation = this.fileStorageLocation.resolve(filename);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
-
             return filename;
-        }catch (Exception e ) {
-            throw new FileStorageException("Could not store file " + ". will be stored!", e);
+        } catch (Exception e) {
+            throw new FileStorageException(
+                    "Could not store file " + filename + ". Please try again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String filename) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(filename).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) return resource;
+            else throw new MyFileNotFoundException("File not found");
+        } catch (Exception e) {
+            throw new MyFileNotFoundException("File not found" + filename, e);
         }
     }
 
