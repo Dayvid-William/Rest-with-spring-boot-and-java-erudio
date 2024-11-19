c380f824662492c1b823ec2090b1b17561de01e8
Dayvid William <dayvid.william5@outlook.com>
2024-11-18 22:31:34 -0300

    finish session 21 upload and download de arquivos

--- api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/Controllers/FileController.java
+++ api-rest-docker/src/main/java/br/com/dayvid/apirestdocker/Controllers/FileController.java
@@ -9,6 +9,12 @@ import java.util.stream.Collectors;
 import br.com.dayvid.apirestdocker.data.vo.v1.UploadFileResponseVO;
 import br.com.dayvid.apirestdocker.services.FileStorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +22,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+
 
 
 @Tag(name = "File Endpoint")
@@ -52,5 +60,30 @@ public class FileController {
                 .stream()
                 .map(file -> uploadFile(file))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{filename:.+}")
+    public ResponseEntity<Resource> downloadFile(
+            @PathVariable String filename, HttpServletRequest request) {
+
+        logger.info("Reading a file on disk");
+
+        Resource resource = service.loadFileAsResource(filename);
+        String contentType = "";
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.info("Could not determine file type!");
+        }
+
+        if (contentType.isBlank()) contentType = "application/octet-stream";
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
